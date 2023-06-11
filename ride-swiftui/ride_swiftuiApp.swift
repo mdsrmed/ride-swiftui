6d2cecb035309ed9d10f4d27431927d617611ed6
mdsrmed <mdsrmed@gmail.com>
2023-06-11 16:02:01 -0400

    Initial Commit

---
+++ ride-swiftui/ride_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ride_swiftuiApp.swift
+//  ride-swiftui
+//
+//  Created by Md Shohidur Rahman on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ride_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
